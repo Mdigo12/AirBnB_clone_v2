97bcd5720c253865affe00f490b8d99aeee288ea
Stanley <jaycobstano@gmail.com>
2023-01-14 02:21:48 -0800

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,8 +13,8 @@ def hello_hbnb():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return HBNB"""
-    return 'HBNB!'
+    return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
