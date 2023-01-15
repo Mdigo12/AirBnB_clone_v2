8a293ad7b9a85866bfc46ff6c5b1695dbec4a5c8
Stanley <jaycobstano@gmail.com>
2023-01-15 09:50:38 -0800

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,19 +17,19 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    text = text.replace('_', " ")
-    """Returns C is fun!"""
-    return 'C {}'.format(text)
+def c_text(text):
+    """ Function called with /c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text):
-    text = text.replace('_', " ")
-    """Returns python is magic!"""
-    return 'python {}'.format(text)
+def python_text(text='is cool'):
+    """ Function called with /python/<text> route """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
