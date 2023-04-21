fccbbcc3ad14053ecdd29bd704a911616ef67e4d
Mdigo12 <mdigojacob@gmail.com>
2023-04-21 15:39:12 +0300

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/pytho3
+"""a flask script to display C and arg passed”"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    """Return Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def index_hbnb():
+    """Return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def index_text(text):
+    """Return HBNB"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def index_python(text='is cool'):
+    """Return HBNB"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def index_number(n):
+    """Return Number passed"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def index_temp(n):
+    return render_template("5-number.html", num=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
