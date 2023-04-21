af48b66bf3d6b212f7b59e5c2341cd9950d1b567
Mdigo12 <mdigojacob@gmail.com>
2023-04-21 14:05:20 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    """Return Hello HBNB"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def index_hbnb():
+    """Return HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def index_text(text):
+    """Return HBNB"""
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
