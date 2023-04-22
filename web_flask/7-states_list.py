ea0cc2776593f1229ed6ac01c101ee58e80c02f2
Mdigo12 <mdigojacob@gmail.com>
2023-04-22 19:44:11 +0300

    flask to list states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Flask script that starts the web application
+Should display a list of all states in the storage
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    states_list = storage.all('States')
+    states_list_sort = sorted(states_list.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states_list_sort)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """removes session after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
