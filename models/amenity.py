bb93140f6b6fab6e562cc43309a33521eb8bed32
Mdigo12 <mdigojacob@gmail.com>
2023-04-22 17:38:42 +0300

    import place and call place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@
 from models.base_model import BaseModel, Base
 from models.city import City
 from models.user import User
-from models.place import place_amenity
+from models import place
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from os import environ
@@ -23,6 +23,6 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
             "Place",
-            secondary=place_amenity, back_populates="amenities")
+            secondary=place.place_amenity, back_populates="amenities")
     else:
         name = ""
