bc064d2c6749d93a487ce98825fc8104b461e2b5
Mdigo12 <mdigojacob@gmail.com>
2023-04-23 11:14:56 +0300

    import State

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,13 +5,18 @@ Should display a list of all states in the storage
 """
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
+
+
 @app.route("/states_list", strict_slashes=False)
 def list_states():
-    states_list = storage.all('States')
-    states_list_sort = sorted(states_list.values(), key=lambda state: state.name)
+    """A function that renders a sorted list of states in db"""
+    states_list = storage.all(State)
+    states_list_sort = sorted(states_list.values(),
+                              key=lambda state: state.name)
+    print(states_list_sort)
     return render_template('7-states_list.html', states=states_list_sort)
 
 
