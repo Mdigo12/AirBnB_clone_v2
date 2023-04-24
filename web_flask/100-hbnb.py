8f701ba7bb954584968d03d336da569c441511d0
Mdigo12 <mdigojacob@gmail.com>
2023-04-24 05:56:08 +0300

    states and amenities

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Flask script that starts the web application
+Should display a list of all states in the storage
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def list_cities_by_states():
+    """A function that gets all the states object from db"""
+    states_list = storage.all(State)
+    amenity_list = storage.all(Amenity)
+    return render_template('100-hbnb.html',
+                           states=states_list, amenities=amenity_list)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """removes session after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
